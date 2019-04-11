597697e0e649c4a7a2224e498e7fbbe47a6b28c5
mtadjdine@yahoo.com <mtadjdine@yahoo.com>
2019-04-11 13:06:59 -0400

    Leaflet initial react starter code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { render } from 'react-dom';
+import { Map, TileLayer, Marker, Popup} from 'react-leaflet';
 
-class App extends Component {
+
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      lat: 40.730610,
+      lng: -73.935242,
+      zoom: 13
+    }
+  }
+
   render() {
+    const position = [this.state.lat, this.state.lng];
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br/> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
     );
   }
 }
 
-export default App;
+render(
+  <App />,
+  document.getElementById('root')
+);
